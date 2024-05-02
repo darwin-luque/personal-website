585ff6101f9574ad4d56474b8a1f61188c18b537
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-05-02 12:54:28 -0600

    chore: remove daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,7 +78,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("daisyui")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
