9e2ea011e51362cee847684d4145071733f1861e
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-05-03 16:24:13 -0600

    fix: properly set next.config for intl and images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import { paraglide } from "@inlang/paraglide-next/plugin";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -5,7 +7,11 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {
+const config = paraglide({
+  paraglide: {
+    project: "./project.inlang",
+    outdir: "./src/paraglide"
+  },
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US",
@@ -18,8 +24,14 @@ const config = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/800x700",
+      },
     ],
   },
-};
+});
 
 export default config;
