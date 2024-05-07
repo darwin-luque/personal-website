dea91fd5268dde261ad18e65941adf5dee609b0b
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-05-07 10:33:52 -0600

    feat: customize progress color in skills

--- src/components/portfolio/skills/skills.tsx
+++ src/components/portfolio/skills/skills.tsx
@@ -2,19 +2,26 @@ import type { FC } from "react";
 import { skills } from "./data";
 import { Progress } from "@/components/ui/progress";
 
+const hexPattern = /^#[0-9A-F]{6}$/i;
+
 export const Skills: FC = () => (
   <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-    {skills.map((skill) => (
-      <div
-        key={skill.id}
-        className="w-full space-y-1 rounded-lg border border-transparent px-4 py-2 hover:border-foreground"
-      >
-        <div className="flex items-center">
-          <div className="mr-2">{skill.icon}</div>
-          <p className={`text-${skill.color}`}>{skill.name}</p>
+    {skills.map((skill) => {
+      const skillColor = skill.color.replace("[", "").replace("]", "");
+      const color = hexPattern.test(skillColor) ? skillColor : undefined;
+
+      return (
+        <div
+          key={skill.id}
+          className="w-full space-y-1 rounded-lg border border-transparent px-4 py-2 hover:border-foreground"
+        >
+          <div className="flex items-center">
+            <div className="mr-2">{skill.icon}</div>
+            <p className={`text-${skill.color}`}>{skill.name}</p>
+          </div>
+          <Progress value={skill.level} className="h-3 w-full" color={color} />
         </div>
-        <Progress value={skill.level} className="h-3 w-full" />
-      </div>
-    ))}
+      );
+    })}
   </div>
 );
--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -8,9 +8,9 @@ import { cn } from "@/lib/utils";
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
-    indicatorClassName?: string;
+    color?: string;
   }
->(({ className, indicatorClassName, value, ...props }, ref) => (
+>(({ className, color, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -20,11 +20,11 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={cn(
-        "h-full w-full flex-1 bg-primary transition-all",
-        indicatorClassName,
-      )}
-      style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
+      className="h-full w-full flex-1 bg-primary transition-all"
+      style={{
+        transform: `translateX(-${100 - (value ?? 0)}%)`,
+        ...(color ? { backgroundColor: color } : {}),
+      }}
     />
   </ProgressPrimitive.Root>
 ));
