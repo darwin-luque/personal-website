568f11ac2adb17f9d1b145baf811994db2b128b4
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-05-07 08:16:19 -0600

    chore: fully localize about me section

--- src/components/homepage/about-me/about-me.tsx
+++ src/components/homepage/about-me/about-me.tsx
@@ -4,6 +4,17 @@ import {
   DraftingCompass,
   ChevronRightIcon,
 } from "lucide-react";
+import {
+  humble,
+  mastery,
+  helpOthers,
+  aboutMeWelcome,
+  seeMyCoreValues,
+  humbleDescription,
+  masteryDescription,
+  aboutMeDescription,
+  helpOthersDescription,
+} from "@/paraglide/messages";
 
 export const AboutMe = () => {
   return (
@@ -11,20 +22,16 @@ export const AboutMe = () => {
       <div className="grid gap-12 md:grid-cols-2">
         <div className="lg:w-3/4">
           <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-            Welcome to my portfolio
+            {aboutMeWelcome()}
           </h2>
           <p className="mt-3 text-muted-foreground">
-            I am a software engineer who loves to build things. I have a
-            background in software development and have been working in the
-            industry for over {new Date().getFullYear() - 2021} years. I have
-            experience in building web applications, mobile applications, and
-            APIs. I am passionate about technology and enjoy learning new
-            things. I am always looking for new challenges and opportunities to
-            grow.
+            {aboutMeDescription({
+              plusYears: new Date().getFullYear() - 2021,
+            })}
           </p>
           <p className="mt-5">
             <span className="group inline-flex items-center gap-x-1 font-medium underline-offset-4">
-              See my core values
+              {seeMyCoreValues()}
               <ChevronRightIcon className="h-4 w-4 flex-shrink-0 transition ease-in-out group-hover:translate-x-1" />
             </span>
           </p>
@@ -35,11 +42,9 @@ export const AboutMe = () => {
               <HeartHandshake className="h-5 w-5 flex-shrink-0" />
             </span>
             <div className="ms-5 sm:ms-8">
-              <h3 className="text-base font-semibold sm:text-lg">Humble</h3>
+              <h3 className="text-base font-semibold sm:text-lg">{humble()}</h3>
               <p className="mt-1 text-muted-foreground">
-                I value humility, embracing a mindset of openness, continuous
-                learning, and self-awareness. It&apos;s about recognizing our
-                strengths while staying grounded and respectful of others.
+                {humbleDescription()}
               </p>
             </div>
           </div>
@@ -49,13 +54,10 @@ export const AboutMe = () => {
             </span>
             <div className="ms-5 sm:ms-8">
               <h3 className="text-base font-semibold sm:text-lg">
-                Help others
+                {helpOthers()}
               </h3>
               <p className="mt-1 text-muted-foreground">
-                I prioritize helping others, striving to make a positive impact in
-                both professional and personal settings. Whether through
-                mentorship, volunteering, or collaboration, I believe in lifting
-                others up and fostering a supportive community.
+                {helpOthersDescription()}
               </p>
             </div>
           </div>
@@ -64,12 +66,11 @@ export const AboutMe = () => {
               <DraftingCompass className="h-5 w-5 flex-shrink-0" />
             </span>
             <div className="ms-5 sm:ms-8">
-              <h3 className="text-base font-semibold sm:text-lg">Mastery</h3>
+              <h3 className="text-base font-semibold sm:text-lg">
+                {mastery()}
+              </h3>
               <p className="mt-1 text-muted-foreground">
-                I pursue mastery, dedicating myself to continuous improvement,
-                innovation, and excellence in my craft. It&apos;s about pushing
-                boundaries, honing skills, and striving for the highest level of
-                proficiency and expertise.
+                {masteryDescription()}
               </p>
             </div>
           </div>
