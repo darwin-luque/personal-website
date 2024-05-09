8fac7604fda708a3e85850d6682ad1642e8b4968
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-05-09 15:59:59 -0600

    wip: create root layout and page and adjust middleware

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,5 +1,3 @@
-import { Inter } from "next/font/google";
-import type { Metadata, Viewport } from "next";
 import type { PropsWithChildren } from "react";
 import { getDictionary } from "@/lib/dictionaries";
 import type { ParamsWithLang } from "@/lib/types";
@@ -7,92 +5,11 @@ import { ThemeProvider } from "@/providers/theme";
 import { Toaster } from "@/components/ui/sonner";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
-import { cn } from "@/lib/utils";
-import "@/styles/globals.css";
+import { i18n } from "@/i18n-config";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
-export const metadata = {
-  title: "Darwin Luque | Software Developer",
-  description:
-    "Explore the engineering portfolio of Darwin Luque, showcasing innovative projects, skills, and qualifications.",
-  keywords: [
-    "Darwin Luque",
-    "Full-Stack Developer",
-    "Web Development",
-    "App Development",
-    "Portfolio",
-    "Projects",
-    "Expertise",
-  ],
-  authors: { name: "Darwin Luque" },
-  creator: "Darwin Luque",
-  icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon.ico", media: "(prefers-color-scheme: dark)" },
-      { url: "/favicon-32x32.png", sizes: "32x32" },
-      { url: "/favicon-16x16.png", sizes: "16x16" },
-    ],
-    apple: [
-      {
-        url: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-    ],
-    other: [
-      {
-        rel: "mask-icon",
-        url: "/safari-pinned-tab.svg",
-        color: "#5bbad5",
-      },
-    ],
-  },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  twitter: {
-    title: "Darwin Luque | Software Developer",
-    description:
-      "Explore the engineering portfolio of Darwin Luque, showcasing innovative projects, skills, and qualifications.",
-    creator: "@dluque_98",
-    images: ["https://nextjs.org/og.png"], // TODO: Add image
-  },
-  appleWebApp: {
-    title: "Darwin Luque | Software Developer",
-    statusBarStyle: "black-translucent",
-    startupImage: [
-      "/apple-touch-image.png",
-      {
-        url: "/apple-touch-image.png",
-        media: "(device-width: 768px) and (device-height: 1024px)",
-      },
-    ],
-  },
-  category: "Software Development",
-  manifest: "/site.webmanifest",
-} satisfies Metadata;
-
-export const viewport = {
-  themeColor: [
-    { media: "(prefers-color-schema: light)", color: "hsl(222.2 47.4% 11.2%)" },
-    { media: "(prefers-color-schema: dark)", color: "hsl(210 40% 98%)" },
-  ],
-} satisfies Viewport;
+export function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ locale }));
+}
 
 export default async function RootLayout({
   children,
@@ -101,27 +18,18 @@ export default async function RootLayout({
   const dict = await getDictionary(lang);
 
   return (
-    <html lang={lang} suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable,
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="relative flex min-h-screen max-w-[100vw] flex-col bg-background">
-            <Navbar lang={lang} dict={dict} />
-            {children}
-            <Footer />
-          </div>
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className="relative flex min-h-screen max-w-[100vw] flex-col bg-background">
+        <Navbar lang={lang} dict={dict} />
+        {children}
+        <Footer />
+      </div>
+      <Toaster />
+    </ThemeProvider>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -24,61 +24,39 @@ function getLocale(request: NextRequest): string | undefined {
 }
 
 export function middleware(request: NextRequest) {
-	console.log('middleware');
 	const pathname = request.nextUrl.pathname;
-	console.log(pathname);
 
 	// // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
 	// // If you have one
-	if (
-		[
-			'/manifest.json',
-			'/favicon.ico',
-			'/android-chrome-192x192.png',
-			'/android-chrome-512x512.png',
-			'/apple-touch-icon.png',
-			'/blog-app-preview.png',
-			'/browserconfig.xml',
-			'/favicon-16x16.png',
-			'/favicon-32x32.png',
-			'/favicon.svg',
-			'/mstile-70x70.png',
-			'/mstile-144x144.png',
-			'/mstile-150x150.png',
-			'/mstile-310x150.png',
-			'/mstile-310x310.png',
-			'/safari-pinned-tab.svg',
-			'/site.webmanifest',
-		].includes(pathname)
-	) {
-		return;
-	}
+	// if (
+	//   [
+	//     '/manifest.json',
+	//     '/favicon.ico',
+	//     // Your other files in `public`
+	//   ].includes(pathname)
+	// )
+	//   return
 
 	// Check if there is any supported locale in the pathname
 	const pathnameIsMissingLocale = i18n.locales.every(
 		(locale) =>
 			!pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
 	);
-	console.log(pathnameIsMissingLocale);
 
 	// Redirect if there is no locale
 	if (pathnameIsMissingLocale) {
 		const locale = getLocale(request);
-		console.log(locale);
 
 		return NextResponse.redirect(
 			new URL(
 				`/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
 				request.url,
 			),
-			{
-				status: 302,
-			},
 		);
 	}
 }
 
 export const config = {
 	// Matcher ignoring `/_next/` and `/api/`
-	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/"],
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
