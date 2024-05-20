d504d673317ef10351e3561fb0c792a1a0aea8a9
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-05-19 20:22:28 -0600

    refactor(hero): make "software developer" stand out

--- src/components/homepage/hero/hero.tsx
+++ src/components/homepage/hero/hero.tsx
@@ -9,6 +9,7 @@ import { Link } from "@/lib/intl";
 
 export const Hero: FC = () => {
   const t = useTranslations("hero");
+  const [name, title] = t("title").split(":");
 
   return (
     <div className="py-12 md:container lg:py-16">
@@ -21,9 +22,13 @@ export const Hero: FC = () => {
           </div>
           <div className="mx-auto mt-5 max-w-2xl">
             <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight md:text-left lg:text-5xl">
-              {t("title")}
+              {name}
+              {": "}
+              <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] bg-clip-text text-transparent">
+                {title}
+              </span>
             </h1>
-            <p className="mt-3 text-xl text-muted-foreground text-center md:text-left">
+            <p className="mt-3 text-center text-xl text-muted-foreground md:text-left">
               {t("description")}
             </p>
           </div>
@@ -32,7 +37,7 @@ export const Hero: FC = () => {
               {t("contactMe")}
             </Link>
           </div>
-          <div className="mt-6 gap-x-5 py-5 lg:mt-10 flex flex-col-reverse md:flex-col">
+          <div className="mt-6 flex flex-col-reverse gap-x-5 py-5 md:flex-col lg:mt-10">
             <Suspense fallback={<HeroGithubStatsLoader />}>
               <HeroGithubStats />
             </Suspense>
